5eb1a87af1bdaec63b7491bfe3408418d56a9142
MaCW99 <maciej.wislawski@gmail.com>
2019-10-18 22:12:57 +0200

    add models

--- charity_donation/donation/models.py
+++ charity_donation/donation/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+
+
+TYPES = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna')
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ManyToManyField(Institution)
+    address = models.CharField(max_length=120)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField(null=True)
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
