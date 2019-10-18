6015021f25d9a60782fa97d062f43b320658c640
MaCW99 <maciej.wislawski@gmail.com>
2019-10-18 23:55:04 +0200

    Landing Page tasks

--- charity_donation/donation/views.py
+++ charity_donation/donation/views.py
@@ -1,12 +1,32 @@
+from django.db.models import Sum, Count
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
 
+from donation.models import Donation, Institution
+
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        bags = Donation.objects.aggregate(Sum('quantity'))
+        # count_inst = Donation.objects.all().annotate(Count("institution", distinct=True))
+
+        count_inst = Donation.objects.values('institution').distinct().count()
+
+        help_fundations = Institution.objects.filter(type=1)
+        help_ngo = Institution.objects.filter(type=2)
+        help_local = Institution.objects.filter(type=3)
+
+        ctx = {
+            'bags': bags,
+            'help_fundations': help_fundations,
+            'help_ngo': help_ngo,
+            'help_local': help_local,
+            'count_inst': count_inst
+
+        }
+        return render(request, 'index.html', ctx)
 
 
 class LoginPage(View):
